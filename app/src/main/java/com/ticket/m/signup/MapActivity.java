a67be85da3ef59f5d68910bf75998e028eb33040
Vishalpcd <vpathak154@gmail.com>
2018-04-02 22:58:41 +0530

    Added all the files

---
+++ app/src/main/java/com/ticket/m/signup/MapActivity.java
@@ -0,0 +1,13 @@
+package com.ticket.m.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActivity extends BasicActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
